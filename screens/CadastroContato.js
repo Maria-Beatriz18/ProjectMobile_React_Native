841b49e25fbaf23f9f8ce7818da87502b102b712
Maria-Beatriz18 <mmbbeatriz.15@gmail.com>
2022-12-10 23:12:17 -0300

    AT04

--- screens/CadastroContato.js
+++ screens/CadastroContato.js
@@ -9,20 +9,20 @@ import axios from 'axios';
 
 const addUser = async (nome, email, telefone, { navigation }) => {
   const result = await axios.post('http://professornilson.com/testeservico/clientes', {
-      nome: nome,
-      email: email,
-      telefone: telefone
+    nome: nome,
+    email: email,
+    telefone: telefone
   })
-  .then((response) => {
+    .then((response) => {
       navigation.goBack();
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
       console.log(error);
-  })
+    })
 }
 
 
-export default function CadastroContato({route,navigation}){
+export default function CadastroContato({ route, navigation }) {
 
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -30,50 +30,53 @@ export default function CadastroContato({route,navigation}){
 
   return (
 
-  <View style={[styles.container, {
-  flexDirection: "column"
-  }]}>
+    <View style={[styles.container, {
+      flexDirection: "column"
+    }]}>
 
 
-<View style={{ flex: 4, alignItems:"center", paddingTop:120}}>
-<Input placeholder='nome'value={nome}
-                onChangeText={nome => setNome(nome)}
-                maxLength={255}/>
-<Input placeholder="email" value={email}
-                onChangeText={email => setEmail(email)}
-                textContentType={'emailAddress'}
-                maxLength={255} />
-<Input placeholder="telefone"value={telefone}
-                onChangeText={telefone => setTelefone(telefone)}
-                textContentType={'telephoneNumber'}
-                maxLength={255} />
-</View>
-  
-  
-<View style={{ flex: 2, alignItems:"center", paddingTop:50}} >
-<Button style={{ marginTop:50}} title="Salvar"  onPress={
-                () => addUser(nome, email, telefone, { navigation })
-            }/>
-</View>
-</View>
-);};
+      <View style={{ flex: 4, alignItems: "center", paddingTop: 120 }}>
+        <Input placeholder='nome' value={nome}
+          leftIcon={{ type: 'font-awesome', name: 'user', color: 'black' }}
+          onChangeText={nome => setNome(nome)}
+          maxLength={255} />
+        <Input placeholder="email" value={email}
+          leftIcon={{ type: 'font-awesome', name: 'envelope', color: 'black' }}
+          onChangeText={email => setEmail(email)}
+          textContentType={'emailAddress'}
+          maxLength={255} />
+        <Input placeholder="telefone" value={telefone}
+          leftIcon={{ type: 'font-awesome', name: 'phone', color: 'black' }}
+          onChangeText={telefone => setTelefone(telefone)}
+          textContentType={'telephoneNumber'}
+          maxLength={255} />
+      </View>
+
+
+      <View style={{ flex: 2, alignItems: "center", paddingTop: 50 }} >
+        <Button style={{ marginTop: 50 }} title="Salvar" onPress={
+          () => addUser(nome, email, telefone, { navigation })
+        } />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-  flex: 1,
-  padding: 20,
+    flex: 1,
+    padding: 20,
   },
-  });
+});
 
 
 
 
 
 
- 
 
 
-  
-  
-  
-  
+
+
+
+
--- screens/Contatos.js
+++ screens/Contatos.js
@@ -1,10 +1,61 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React,{ useEffect,useState } from 'react';
+import { StyleSheet, View } from "react-native";
 import { Input } from 'react-native-elements';
 import { Button } from 'react-native-elements';
+import axios from 'axios';
 
 export default function Contatos({route,navigation}){
 
+  const [getNome, setNome] =useState("");
+  const [getEmail, setEmail] =useState("");
+  const [getTelefone, setTelefone] = useState("");
+  const [getId, setId] = useState("");
+  
+
+  useEffect(()=>{
+    if(route.params){
+          const { nome } =  route.params; 
+          const { email } = route.params;
+          const { telefone } =  route.params; 
+          const { id } =  route.params;
+          
+          setNome(nome);
+          setEmail(email);
+          setTelefone(telefone);
+          setId(id);
+         
+    }
+  },[]) 
+
+  function excluirDados(){
+    axios.delete('http://professornilson.com/testeservico/clientes/'+getId
+   )
+   .then(function (response) {
+       setNome('')
+       setEmail('')
+       setTelefone('')
+      
+     console.log(response);
+   })
+   .catch(function (error) {
+       console.log(error);
+   });
+}
+
+async function alterarDados(){
+await axios.put('http://professornilson.com/testeservico/clientes/'+getId,{
+ nome:getNome,
+ email:getEmail,
+ telefone:getTelefone,
+}
+)
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+    console.log(error);
+});
+}
 
   return (
 
@@ -14,18 +65,27 @@ export default function Contatos({route,navigation}){
 
 
 <View style={{ flex: 2, alignItems:"center", paddingTop:120}}>
-<Input placeholder='nome'/>
-<Input placeholder="email"  />
-<Input placeholder="telefone"  />
+<Input placeholder='nome'
+onChangeText={text => setNome(text)} value={getNome}
+leftIcon={{ type: 'font-awesome', name: 'user', color: 'black' }}
+/>
+<Input placeholder='email'
+onChangeText={text => setEmail(text)} value={getEmail} 
+leftIcon={{ type: 'font-awesome', name: 'envelope', color: 'black' }}
+/>
+<Input placeholder='telefone'
+onChangeText={text => setTelefone(text)} value={getTelefone}
+leftIcon={{ type: 'font-awesome', name: 'phone', color: 'black' }}
+/>
 </View>
   
   
 <View >
-<Button style={{ marginTop:50}} title="alterar"/>
+<Button style={{ marginTop:50}} title="alterar"  onPress={()=>alterarDados()}/>
 </View>
 <View style={{ paddingTop:20}}>
 <Button style={{ marginTop:50}} title="excluir"
- buttonStyle={{backgroundColor: 'red'}} />
+ buttonStyle={{backgroundColor: 'red'}} onPress={()=>excluirDados()} />
 </View>
 </View>
 );};
